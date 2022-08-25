e017c981978f30c97155515e72db11442557d49a
jason mwaura mbugua <jason.mwaura@student.moringaschool.com>
2022-08-25 16:07:54 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+//Looping lab
+//jason
+const sinon= require ("sinon")
+beforeEach( () => {
+    spy = sinon.spy( console, 'log' );
+  } );
+
+  afterEach( () => {
+    spy.restore();
+  } );
+
+function writeCards( [jason,Alice,Tito,LucyJane,Micheal], event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+  function countDown( countdown ) 
+    while ( countdown > 0 ) 
+      console.log( countdown );
+      countdown -= 1;
+  function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( countdown );
+    console.log( startingNumber );
+  }
